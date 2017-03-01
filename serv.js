d0578232287975aa490791f9d37c96f1d64f0143
boatbot12 <boat.bom@gmail.com>
2017-03-01 23:46:37 +0700

    Revert "V.1.1.1"
    This reverts commit b2bef3b1e2924005082b1f2ec64b9cdeed37c46d.

--- serv.js
+++ serv.js
@@ -1,21 +1,34 @@
 var http = require("http") ;
+var fs = require("fs") ;
 
 var server = http.createServer(function(req, res){
   if(req.url == '/'){
-    var data = "<h1>abc</h1>";
+    var data = fs.readFileSync("./game_play/game.html");
     res.writeHead(200, {
-      'Content-Type': 'text/plain',
+      'Content-Type': 'text/html',
       'Content-Length': data.length
     });
-    res.end(data, function(){
-      console.log("Call /");
-    });
+    res.end(data);
   } else if(req.url == '/index'){
     res.writeHead(302, {
       'Location': '/'
     });
     res.end();
-  } else {
+  } else if(req.url.match(/\/png\//)){
+    var data = fs.readFileSync("./game_play"+req.url);
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': data.length
+    });
+    res.end(data);
+  } else if(req.url == '/Hw.css'){
+    var data = fs.readFileSync("./game_play/Hw.css");
+    res.writeHead(200, {
+      'Content-Type': 'text/css',
+      'Content-Length': data.length
+    });
+    res.end(data);
+  }else {
     var data = "<h1>404 Not found</h1>";
     res.writeHead(404, {
       'Content-Type': 'text/html',
@@ -28,5 +41,4 @@ var server = http.createServer(function(req, res){
 });
 
 server.listen(3000, function(){
-  console.log('Node start');
 });
